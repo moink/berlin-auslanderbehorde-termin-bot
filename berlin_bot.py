4799de64d04c0463b92ad14eb7da228953a96a61
moink <theresa.robinson@gmail.com>
2023-05-14 12:52:35 +0200

    refactor out constants

--- berlin_bot.py
+++ berlin_bot.py
@@ -8,114 +8,149 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
+# Settings applicable to an individual
+CITIZENSHIP_COUNTRY = "Kanada"
+PERSON_COUNT = "eine Person"
+NOT_WITH_FAMILY = "nein"
+EXTEND_RESIDENCE_PERMIT_BUTTON = '//*[@id="xi-div-30"]/div[2]/label/p'
+FAMILY_GROUNDS_ACCORDION = '//*[@id="inner-348-0-2"]/div/div[5]/label/p'
+FAMILY_OF_SKILLED_WORKER_RADIO_BUTTON = (
+    '//*[@id="inner-348-0-2"]/div/div[6]/div/div[1]/label'
+)
+
+# General configuration
+START_PAGE_URL = "https://otv.verwalt-berlin.de/ams/TerminBuchen"
+WEBSITE_ERROR_RESPONSE = (
+    """Für die gewählte Dienstleistung sind aktuell keine Termine frei! Bitte"""
+)
+WAV_FILE_PLAYER = "/usr/bin/aplay"
+SOUND_FILE_NAME = "alarm.wav"
+SOUND_FILE_LENGTH = 15  # in seconds
 INNER_RETRY_ATTEMPTS = 2
+LOGGING_FORMAT = '%(asctime)s\t%(levelname)s\t%(message)s'
+
+# Driver setup options
+USER_AGENT_SETTINGS = (
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+    'AppleWebKit/537.36 (KHTML, like Gecko) ' 
+    'Chrome/83.0.4103.53 Safari/537.36'
+)
+DISABLE_BLINK_OPTION = '--disable-blink-features=AutomationControlled'
+SET_WEBDRIVER_UNDEFINED_SCRIPT = (
+    "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"
+)
+
+# Wait times used - all in seconds
+TERMS_AGREEMENT_WAIT_TIME = 1
+POST_BUTTON_CLICK_WAIT_TIME = 2
+POST_RADIO_BUTTON_CLICK_WAIT_TIME = 4
+PAGE_LOAD_WAIT_TIME = 5
+FORM_SUBMISSION_WAIT_TIME = 10
+WAIT_TIME_BETWEEN_ATTEMPTS = 20
+IMPLICIT_WAIT_TIME = 20
+
+# DOM elements needed for clicking
+BOOK_APPT_BUTTON = (
+    '//*[@id="mainForm"]/div/div/div/div/div/div/div/div/div/div[1]/div[1]/div[2]/a'
+)
+TERMS_AGREEMENT_CHECKBOX = '//*[@id="xi-div-1"]/div[4]/label[2]/p'
+CITIZENSHIP_DROPDOWN = 'xi-sel-400'
+PERSON_COUNT_DROPDOWN = 'xi-sel-422'
+WITH_FAMILY_DROPDOWN = 'xi-sel-427'
+PROCEED_BUTTON = 'applicationForm:managedForm:proceed'
 
 system = system()
 
 logging.basicConfig(
-    format='%(asctime)s\t%(levelname)s\t%(message)s',
+    format=LOGGING_FORMAT,
     level=logging.INFO,
 )
 
+
 class WebDriver:
-    def __init__(self):
-        self._driver: webdriver.Chrome
-        self._implicit_wait_time = 20
 
     def __enter__(self) -> webdriver.Chrome:
         logging.info("Open browser")
         # some stuff that prevents us from being locked out
-        options = webdriver.ChromeOptions() 
-        options.add_argument('--disable-blink-features=AutomationControlled')
+        options = webdriver.ChromeOptions()
+        options.add_argument(DISABLE_BLINK_OPTION)
         self._driver = webdriver.Chrome(options=options)
-        self._driver.implicitly_wait(self._implicit_wait_time) # seconds
-        self._driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-        self._driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+        self._driver.implicitly_wait(IMPLICIT_WAIT_TIME)
+        self._driver.execute_script(SET_WEBDRIVER_UNDEFINED_SCRIPT)
+        self._driver.execute_cdp_cmd(
+            'Network.setUserAgentOverride',
+            {"userAgent": USER_AGENT_SETTINGS})
         return self._driver
 
     def __exit__(self, exc_type, exc_value, exc_tb):
         logging.info("Close browser")
         self._driver.quit()
 
+
 class BerlinBot:
     def __init__(self):
-        self.wait_time = 20
-        self._sound_file = os.path.join(os.getcwd(), "alarm.wav")
-        self._error_message = """Für die gewählte Dienstleistung sind aktuell keine Termine frei! Bitte"""
+        self._error_message = WEBSITE_ERROR_RESPONSE
 
     @staticmethod
     def enter_start_page(driver: webdriver.Chrome):
         logging.info("Visit start page")
-        driver.get("https://otv.verwalt-berlin.de/ams/TerminBuchen")
-        driver.find_element(By.XPATH, '//*[@id="mainForm"]/div/div/div/div/div/div/div/div/div/div[1]/div[1]/div[2]/a').click()
-        time.sleep(5)
+        driver.get(START_PAGE_URL)
+        driver.find_element(By.XPATH, BOOK_APPT_BUTTON).click()
+        time.sleep(PAGE_LOAD_WAIT_TIME)
 
     @staticmethod
-    def tick_off_some_bullshit(driver: webdriver.Chrome):
+    def accept_terms(driver: webdriver.Chrome):
         logging.info("Ticking off agreement")
-        driver.find_element(By.XPATH, '//*[@id="xi-div-1"]/div[4]/label[2]/p').click()
-        time.sleep(1)
-        driver.find_element(By.ID, 'applicationForm:managedForm:proceed').click()
-        time.sleep(5)
+        driver.find_element(By.XPATH, TERMS_AGREEMENT_CHECKBOX).click()
+        time.sleep(TERMS_AGREEMENT_WAIT_TIME)
+        driver.find_element(By.ID, PROCEED_BUTTON).click()
+        time.sleep(PAGE_LOAD_WAIT_TIME)
 
     @staticmethod
     def enter_form(driver: webdriver.Chrome):
         logging.info("Fill out form")
-        # select china
-        s = Select(driver.find_element(By.ID, 'xi-sel-400'))
-        s.select_by_visible_text("Kanada")
-        # eine person
-        s = Select(driver.find_element(By.ID, 'xi-sel-422'))
-        s.select_by_visible_text("eine Person")
-        # no family
-        s = Select(driver.find_element(By.ID, 'xi-sel-427' ))
-        s.select_by_visible_text("nein")
-        time.sleep(5)
+        s = Select(driver.find_element(By.ID, CITIZENSHIP_DROPDOWN))
+        s.select_by_visible_text(CITIZENSHIP_COUNTRY)
+        s = Select(driver.find_element(By.ID, PERSON_COUNT_DROPDOWN))
+        s.select_by_visible_text(PERSON_COUNT)
+        s = Select(driver.find_element(By.ID, WITH_FAMILY_DROPDOWN))
+        s.select_by_visible_text(NOT_WITH_FAMILY)
+        time.sleep(PAGE_LOAD_WAIT_TIME)
+        driver.find_element(By.XPATH, EXTEND_RESIDENCE_PERMIT_BUTTON).click()
+        time.sleep(POST_BUTTON_CLICK_WAIT_TIME)
+        driver.find_element(By.XPATH, FAMILY_GROUNDS_ACCORDION).click()
+        time.sleep(POST_BUTTON_CLICK_WAIT_TIME)
+        driver.find_element(By.XPATH, FAMILY_OF_SKILLED_WORKER_RADIO_BUTTON).click()
+        time.sleep(POST_RADIO_BUTTON_CLICK_WAIT_TIME)
+        driver.find_element(By.ID, PROCEED_BUTTON).click()
+        time.sleep(FORM_SUBMISSION_WAIT_TIME)
 
-        # extend stay
-        driver.find_element(By.XPATH, '//*[@id="xi-div-30"]/div[2]/label/p').click()
-        time.sleep(2)
-
-        # click on family group
-        driver.find_element(By.XPATH, '//*[@id="inner-348-0-2"]/div/div[5]/label/p').click()
-        time.sleep(2)
-
-        #
-        driver.find_element(By.XPATH, '//*[@id="inner-348-0-2"]/div/div[6]/div/div[1]/label').click()
-        time.sleep(4)
-
-        # submit form
-        driver.find_element(By.ID, 'applicationForm:managedForm:proceed').click()
-        time.sleep(10)
-    
-    def _success(self):
+    @staticmethod
+    def _success():
         logging.info("!!!SUCCESS - do not close the window!!!!")
         while True:
-            subprocess.run(["/usr/bin/aplay", self._sound_file])
-            time.sleep(15)
-        
-        # todo play something and block the browser
-
+            sound_file_path = os.path.join(os.getcwd(), SOUND_FILE_NAME)
+            subprocess.run([WAV_FILE_PLAYER, sound_file_path])
+            time.sleep(SOUND_FILE_LENGTH)
 
     def run_once(self):
         with WebDriver() as driver:
             self.enter_start_page(driver)
-            self.tick_off_some_bullshit(driver)
+            self.accept_terms(driver)
             self.enter_form(driver)
-
             # retry submit
             for _ in range(INNER_RETRY_ATTEMPTS):
                 if not self._error_message in driver.page_source:
                     self._success()
                 logging.info("Retry submitting form")
-                driver.find_element(By.ID, 'applicationForm:managedForm:proceed').click()
-                time.sleep(self.wait_time)
+                driver.find_element(By.ID, PROCEED_BUTTON).click()
+                time.sleep(WAIT_TIME_BETWEEN_ATTEMPTS)
 
     def run_loop(self):
         while True:
             logging.info("One more round")
             self.run_once()
-            time.sleep(self.wait_time)
+            time.sleep(WAIT_TIME_BETWEEN_ATTEMPTS)
 
 
 if __name__ == "__main__":

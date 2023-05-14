5ef516a01c8eaff9b312e335a7d67898bdd4286d
moink <theresa.robinson@gmail.com>
2023-05-14 13:16:42 +0200

    remove unneeded class. put log messages in consistent tense. reorder functions into newspaper order

--- berlin_bot.py
+++ berlin_bot.py
@@ -9,12 +9,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
 # Settings applicable to an individual
-CITIZENSHIP_COUNTRY = "Kanada"
-PERSON_COUNT = "eine Person"
-NOT_WITH_FAMILY = "nein"
-EXTEND_RESIDENCE_PERMIT_BUTTON = '//*[@id="xi-div-30"]/div[2]/label/p'
-FAMILY_GROUNDS_ACCORDION = '//*[@id="inner-348-0-2"]/div/div[5]/label/p'
-FAMILY_OF_SKILLED_WORKER_RADIO_BUTTON = (
+CITIZENSHIP_COUNTRY_TEXT = "Kanada"
+PERSON_COUNT_TEXT = "eine Person"
+NOT_WITH_FAMILY_TEXT = "nein"
+EXTEND_RESIDENCE_PERMIT_BUTTON_XPATH = '//*[@id="xi-div-30"]/div[2]/label/p'
+FAMILY_GROUNDS_ACCORDION_XPATH = '//*[@id="inner-348-0-2"]/div/div[5]/label/p'
+FAMILY_OF_SKILLED_WORKER_RADIO_BUTTON_XPATH = (
     '//*[@id="inner-348-0-2"]/div/div[6]/div/div[1]/label'
 )
 
@@ -50,14 +50,14 @@ WAIT_TIME_BETWEEN_ATTEMPTS = 20
 IMPLICIT_WAIT_TIME = 20
 
 # DOM elements needed for clicking
-BOOK_APPT_BUTTON = (
+BOOK_APPT_BUTTON_XPATH = (
     '//*[@id="mainForm"]/div/div/div/div/div/div/div/div/div/div[1]/div[1]/div[2]/a'
 )
-TERMS_AGREEMENT_CHECKBOX = '//*[@id="xi-div-1"]/div[4]/label[2]/p'
-CITIZENSHIP_DROPDOWN = 'xi-sel-400'
-PERSON_COUNT_DROPDOWN = 'xi-sel-422'
-WITH_FAMILY_DROPDOWN = 'xi-sel-427'
-PROCEED_BUTTON = 'applicationForm:managedForm:proceed'
+TERMS_AGREEMENT_CHECKBOX_XPATH = '//*[@id="xi-div-1"]/div[4]/label[2]/p'
+CITIZENSHIP_DROPDOWN_ID = 'xi-sel-400'
+PERSON_COUNT_DROPDOWN_ID = 'xi-sel-422'
+WITH_FAMILY_DROPDOWN_ID = 'xi-sel-427'
+PROCEED_BUTTON_ID = 'applicationForm:managedForm:proceed'
 
 system = system()
 
@@ -65,6 +65,69 @@ logging.basicConfig(
     format=LOGGING_FORMAT,
     level=logging.INFO,
 )
+
+
+def run_loop():
+    while True:
+        logging.info("Start appointment arrangement attempt")
+        run_once()
+        time.sleep(WAIT_TIME_BETWEEN_ATTEMPTS)
+
+
+def run_once():
+    with WebDriver() as driver:
+        enter_start_page(driver)
+        accept_terms(driver)
+        enter_form(driver)
+        # retry submit
+        for _ in range(INNER_RETRY_ATTEMPTS):
+            if not WEBSITE_ERROR_RESPONSE in driver.page_source:
+                alert_user()
+            logging.info("Retry submitting form")
+            driver.find_element(By.ID, PROCEED_BUTTON_ID).click()
+            time.sleep(WAIT_TIME_BETWEEN_ATTEMPTS)
+
+
+def enter_start_page(driver: webdriver.Chrome):
+    logging.info("Visit start page")
+    driver.get(START_PAGE_URL)
+    driver.find_element(By.XPATH, BOOK_APPT_BUTTON_XPATH).click()
+    time.sleep(PAGE_LOAD_WAIT_TIME)
+
+
+def accept_terms(driver: webdriver.Chrome):
+    logging.info("Tick off agreement")
+    driver.find_element(By.XPATH, TERMS_AGREEMENT_CHECKBOX_XPATH).click()
+    time.sleep(TERMS_AGREEMENT_WAIT_TIME)
+    driver.find_element(By.ID, PROCEED_BUTTON_ID).click()
+    time.sleep(PAGE_LOAD_WAIT_TIME)
+
+
+def enter_form(driver: webdriver.Chrome):
+    logging.info("Fill out form")
+    s = Select(driver.find_element(By.ID, CITIZENSHIP_DROPDOWN_ID))
+    s.select_by_visible_text(CITIZENSHIP_COUNTRY_TEXT)
+    s = Select(driver.find_element(By.ID, PERSON_COUNT_DROPDOWN_ID))
+    s.select_by_visible_text(PERSON_COUNT_TEXT)
+    s = Select(driver.find_element(By.ID, WITH_FAMILY_DROPDOWN_ID))
+    s.select_by_visible_text(NOT_WITH_FAMILY_TEXT)
+    time.sleep(PAGE_LOAD_WAIT_TIME)
+    driver.find_element(By.XPATH, EXTEND_RESIDENCE_PERMIT_BUTTON_XPATH).click()
+    time.sleep(POST_BUTTON_CLICK_WAIT_TIME)
+    driver.find_element(By.XPATH, FAMILY_GROUNDS_ACCORDION_XPATH).click()
+    time.sleep(POST_BUTTON_CLICK_WAIT_TIME)
+    driver.find_element(By.XPATH, FAMILY_OF_SKILLED_WORKER_RADIO_BUTTON_XPATH).click()
+    time.sleep(POST_RADIO_BUTTON_CLICK_WAIT_TIME)
+    driver.find_element(By.ID, PROCEED_BUTTON_ID).click()
+    time.sleep(FORM_SUBMISSION_WAIT_TIME)
+
+
+def alert_user():
+    logging.info("!!!SUCCESS - do not close the window!!!!")
+    sound_file_path = os.path.join(os.getcwd(), SOUND_FILE_NAME)
+    while True:
+        subprocess.run([WAV_FILE_PLAYER, sound_file_path])
+        time.sleep(SOUND_FILE_LENGTH)
 
 
 class WebDriver:
@@ -87,71 +150,5 @@ class WebDriver:
         self._driver.quit()
 
 
-class BerlinBot:
-    def __init__(self):
-        self._error_message = WEBSITE_ERROR_RESPONSE
-
-    @staticmethod
-    def enter_start_page(driver: webdriver.Chrome):
-        logging.info("Visit start page")
-        driver.get(START_PAGE_URL)
-        driver.find_element(By.XPATH, BOOK_APPT_BUTTON).click()
-        time.sleep(PAGE_LOAD_WAIT_TIME)
-
-    @staticmethod
-    def accept_terms(driver: webdriver.Chrome):
-        logging.info("Ticking off agreement")
-        driver.find_element(By.XPATH, TERMS_AGREEMENT_CHECKBOX).click()
-        time.sleep(TERMS_AGREEMENT_WAIT_TIME)
-        driver.find_element(By.ID, PROCEED_BUTTON).click()
-        time.sleep(PAGE_LOAD_WAIT_TIME)
-
-    @staticmethod
-    def enter_form(driver: webdriver.Chrome):
-        logging.info("Fill out form")
-        s = Select(driver.find_element(By.ID, CITIZENSHIP_DROPDOWN))
-        s.select_by_visible_text(CITIZENSHIP_COUNTRY)
-        s = Select(driver.find_element(By.ID, PERSON_COUNT_DROPDOWN))
-        s.select_by_visible_text(PERSON_COUNT)
-        s = Select(driver.find_element(By.ID, WITH_FAMILY_DROPDOWN))
-        s.select_by_visible_text(NOT_WITH_FAMILY)
-        time.sleep(PAGE_LOAD_WAIT_TIME)
-        driver.find_element(By.XPATH, EXTEND_RESIDENCE_PERMIT_BUTTON).click()
-        time.sleep(POST_BUTTON_CLICK_WAIT_TIME)
-        driver.find_element(By.XPATH, FAMILY_GROUNDS_ACCORDION).click()
-        time.sleep(POST_BUTTON_CLICK_WAIT_TIME)
-        driver.find_element(By.XPATH, FAMILY_OF_SKILLED_WORKER_RADIO_BUTTON).click()
-        time.sleep(POST_RADIO_BUTTON_CLICK_WAIT_TIME)
-        driver.find_element(By.ID, PROCEED_BUTTON).click()
-        time.sleep(FORM_SUBMISSION_WAIT_TIME)
-
-    @staticmethod
-    def _success():
-        logging.info("!!!SUCCESS - do not close the window!!!!")
-        while True:
-            sound_file_path = os.path.join(os.getcwd(), SOUND_FILE_NAME)
-            subprocess.run([WAV_FILE_PLAYER, sound_file_path])
-            time.sleep(SOUND_FILE_LENGTH)
-
-    def run_once(self):
-        with WebDriver() as driver:
-            self.enter_start_page(driver)
-            self.accept_terms(driver)
-            self.enter_form(driver)
-            # retry submit
-            for _ in range(INNER_RETRY_ATTEMPTS):
-                if not self._error_message in driver.page_source:
-                    self._success()
-                logging.info("Retry submitting form")
-                driver.find_element(By.ID, PROCEED_BUTTON).click()
-                time.sleep(WAIT_TIME_BETWEEN_ATTEMPTS)
-
-    def run_loop(self):
-        while True:
-            logging.info("One more round")
-            self.run_once()
-            time.sleep(WAIT_TIME_BETWEEN_ATTEMPTS)
-
-
 if __name__ == "__main__":
-    BerlinBot().run_loop()
+    run_loop()
